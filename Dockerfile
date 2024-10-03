644ea4260234ab8b4194b09185c322c4b6f42d67
gonzalo-olmedo <gonzaloabelolmedo8@gmail.com>
2024-10-03 15:54:19 -0300

    Subiendo repositorio de Django

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+# Usa una imagen base de Python 3.12
+FROM python:3.12
+
+# Establece el directorio de trabajo en /planetsuperheroes
+WORKDIR /planetsuperheroes
+
+# Copia el archivo .env al contenedor
+COPY .env /planetsuperheroes/.env
+
+# Copia todos los archivos al contenedor
+COPY . .
+
+# Crea el directorio de logs
+RUN mkdir -p /planetsuperheroes/logs
+
+# Instala las dependencias del proyecto
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Comando para recopilar archivos estáticos
+RUN python manage.py collectstatic --noinput
+
+# Instala psycopg2-binary
+RUN pip install psycopg2-binary
+
+# Ejecuta las migraciones automáticamente
+RUN python manage.py migrate
+# Expone el puerto de la aplicación
+EXPOSE 8000
+
+# Comando para ejecutar la aplicación con Gunicorn y especificar el archivo de log de errores
+CMD ["gunicorn", "universidad.wsgi:application", "--bind", "0.0.0.0:8000", "--error-logfile", "/planetsuperheroes/logs/django_error.log"]
---
+++ universidad/settings.py
@@ -0,0 +1,169 @@
+import os
+from datetime import timedelta
+from pathlib import Path
+from dotenv import load_dotenv
+import dj_database_url
+
+
+load_dotenv()
+
+# Ruta base del proyecto
+BASE_DIR = Path(__file__).resolve().parent.parent
+WSGI_APPLICATION = 'universidad.wsgi.application'
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-0e*4)xp%lp!2lc37aujo38n-14a@4wo81qqjsi-!atniye0jd$'
+
+
+ALLOWED_HOSTS = ['*']
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+AUTH_USER_MODEL = 'MyComicApp.User'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'ERROR',
+            'class': 'logging.FileHandler',
+            'filename': os.path.join(BASE_DIR, 'logs/django_error.log'),
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'ERROR',
+            'propagate': True,
+        },
+    },
+}
+
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'MyComicApp',
+    'rest_framework',
+    'corsheaders',
+    'drf_yasg',
+    'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    # 'rest_framework_simplejwt.authentication.JWTAuthentication',
+]
+CORS_ORIGIN_WHITELIST = ['http://localhost:4200']
+CORS_ALLOWED_ORIGINS = ['http://localhost:4200',]
+CORS_ALLOW_CREDENTIALS = True
+ROOT_URLCONF = 'universidad.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+# Database
+DATABASES = {
+    'default': {
+        'default': dj_database_url.config(default=os.getenv('DATABASE_URL')),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
+    }
+}
+
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_TZ = True
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+ 
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES':(
+        'rest_framework.permissions.AllowAny',
+    )
+}
+
+SIMPLE_JWT = {
+    'ROTATE_REFRESH_TOKENS':True,
+    'BLACKLIST_AFTER_ROTATION':True,
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=255),
+    'REFRESH_TOKEN_LIFETIME':timedelta(days=1),
+       
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',   
+}
+
+APPEND_SLASH = False
