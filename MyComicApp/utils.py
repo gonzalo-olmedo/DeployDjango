6e46d6431ce117bc9678d56a7c7b48b9e7309402
gonzalo-olmedo <gonzaloabelolmedo8@gmail.com>
2024-10-05 23:46:02 -0300

    Ignorar .env

--- MyComicApp/utils.py
+++ MyComicApp/utils.py
@@ -2,16 +2,11 @@ import os
 import uuid
 
 def generate_public_id(instance, filename):
-    # Obtener el nombre base y la extensión
+
     name, ext = os.path.splitext(filename)
 
-    # Reemplazar espacios y caracteres especiales
     name = name.replace(' ', '_').replace('-', '_').lower()
 
-    # Generar un UUID para asegurar unicidad
-    unique_id = uuid.uuid4().hex
+    public_id = f"planetsuperheroes/images/productos/{name}{ext}"
 
-    # Combinar el nombre y el UUID con la ruta deseada
-    public_id = f"planetsuperheroes/images/productos/{name}_{unique_id}{ext}"
-
-    return public_id  # Devuelve el ID público generado
+    return public_id  
