b0a8f5cda1aca7f9572169c6b7ba4b35a980c7f1
gonzalo-olmedo <gonzaloabelolmedo8@gmail.com>
2024-10-12 15:43:25 -0300

    Modificacion de public id

--- MyComicApp/models.py
+++ MyComicApp/models.py
@@ -117,14 +117,17 @@ class Product(models.Model):
 
     def save(self, *args, **kwargs):
         if self.image:
-            # Generar el public_id basado en el nombre del archivo de la imagen
             public_id = generate_public_id(self, self.image.name)
 
-            # Usar el método de Cloudinary para subir la imagen
-            upload_result = cloudinary.uploader.upload(self.image, public_id=public_id)
+            try:
+                # Subir la imagen a Cloudinary
+                upload_result = cloudinary.uploader.upload(self.image, public_id=public_id)
 
-            # Asignar la URL de la imagen a self.image
-            self.image = upload_result['secure_url']  # O usar 'url' si prefieres la no segura
+                # Asignar la URL de la imagen a self.image
+                self.image = upload_result['secure_url']
+            except Exception as e:
+                print(f"Error al subir la imagen a Cloudinary: {e}")  # Esto te ayudará a ver qué está fallando
+                raise  # Lanza el error nuevamente después de imprimirlo
 
         super().save(*args, **kwargs)
 
--- MyComicApp/utils.py
+++ MyComicApp/utils.py
@@ -1,4 +1,5 @@
 import os
+import uuid
 import cloudinary.uploader
 from django.db import models
 from cloudinary.models import CloudinaryField
@@ -7,8 +8,7 @@ def generate_public_id(instance, filename):
     name, ext = os.path.splitext(filename)
     name = name.replace(' ', '_').replace('-', '_').lower()
 
-    # Generar un ID único utilizando uuid
-    unique_id = str(uuid.uuid4())  # Esto generará un ID único
+    unique_id = str(uuid.uuid4())
     public_id = f"planetsuperheroes/images/productos/{name}_{unique_id}{ext}"
 
     return public_id
